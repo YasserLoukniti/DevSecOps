22270d610e7d79573940e0f2497482b981f4e8aa
Yasser Loukniti <yasser.loukniti@estiam.com>
2022-11-10 16:56:04 +0100

    Tests witout failure

--- __tests/input.test.js
+++ __tests/input.test.js
@@ -11,8 +11,3 @@ describe("Test if user enters an acceptable user input which is alphanumeric", f
     expect(isAlphaNumeric('ThisIsAnAlphanumeric#$$@@')).toEqual(false);
     });
 });
-describe("Test if user enters an acceptable user input which is alphanumeric", function () {
-    it("Should test if user enters alphanumeric or not", function () {
-    expect(isAlphaNumeric('Yasser@')).toEqual(true);
-    });
-});
